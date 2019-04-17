4e524c8d39953da0d9a810a42064f33bd8eaa3a5
matsalen <k.kurosaki@hotmail.com>
2019-04-17 17:36:25 -0300

    Comite feito pela menina mais legal que você conhece: THAMIRES MASSIDELLI DA SILVA

--- Jogo2DAlone/Jogo2DAlone/Assets/Scripts/RobotController.cs
+++ Jogo2DAlone/Jogo2DAlone/Assets/Scripts/RobotController.cs
@@ -7,14 +7,26 @@ public class RobotController : MonoBehaviour
     [HideInInspector] public bool ViradoDireita = true;
     public float h;
     public float forcaAceleracao = 5f;
+    public float ForcaPulo = 350f;
+    public float RaioSolo = 0.2f;
+    public bool NoSolo = false;
     public Rigidbody2D r;
     public Animator anim;
+    public LayerMask ESolo;
+    public Transform VerificaSolo;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         r = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        NoSolo = Physics2D.OverlapCircle(VerificaSolo.position, RaioSolo, ESolo);
+        anim.SetBool("Chao", NoSolo);
+        anim.SetFloat("SpeedVert", Mathf.Abs(r.velocity.y));
     }
 
     // Update is called once per frame
@@ -24,11 +36,19 @@ public class RobotController : MonoBehaviour
 
         r.velocity = new Vector2(h * forcaAceleracao, r.velocity.y);
 
-        anim.SetFloat("Speed", h);
+        anim.SetFloat("Speed", Mathf.Abs(r.velocity.x));
 
-        if (ViradoDireita)
+        if (ViradoDireita && h < 0)
         {
-
+            Flip();
+        }
+        else if (!ViradoDireita && h > 0)
+        {
+            Flip();
+        }
+        if (NoSolo & Input.GetButtonDown("Jump"))
+        {
+            r.AddForce(new Vector2(0, ForcaPulo));
         }
         
     }
@@ -40,4 +60,5 @@ public class RobotController : MonoBehaviour
         escala.x *= -1;
         transform.localScale = escala;
     }
+    
 }
